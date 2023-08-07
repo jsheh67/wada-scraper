e4750fe3651f88e1a741714942fc1a73cfd2ac67
joesh <joe.shehr@gmail.co>
2023-08-07 18:47:04 -0500

    seconds to string for marathon

--- src/main/java/org/example/utilities/TimeConverter.java
+++ src/main/java/org/example/utilities/TimeConverter.java
@@ -21,17 +21,32 @@ public class TimeConverter {
         return seconds;
     }
 
+    public static String getString(float seconds){
+        //half marathon and marathon times are rounded to the second
+        if(seconds>(60*60)){
+            int secondsRound = Math.round(seconds);
+            int hours = (int)Math.floor(secondsRound/(3600));
+            int minutes = (int)Math.floor((secondsRound%3600)/60);
+            int secondsLeft = secondsRound-(hours*3600)-(minutes*60);
+
+            System.out.println(hours+":"+minutes+":"+secondsLeft);
+        }
+        return "test";
+    }
+
 
     public static void main(String[] args){
-        String sprint = "10.65";
-        String minute="1:55.77";
-        String dMinute="14:00.20";
-        String hour="2:16:28";
+//        String sprint = "10.65";
+//        String minute="1:55.77";
+//        String dMinute="14:00.20";
+       String hour="2:16:28";
+//
+           System.out.println(getSeconds(hour)+"---"+hour);
+//        System.out.println(getSeconds(dMinute)+"---"+dMinute);
+//        System.out.println(getSeconds(minute)+"---"+minute);
+//        System.out.println(getSeconds(sprint)+"---"+sprint);
 
-        System.out.println(getSeconds(hour)+"---"+hour);
-        System.out.println(getSeconds(dMinute)+"---"+dMinute);
-        System.out.println(getSeconds(minute)+"---"+minute);
-        System.out.println(getSeconds(sprint)+"---"+sprint);
+        getString(8188);
     }
 }
 
